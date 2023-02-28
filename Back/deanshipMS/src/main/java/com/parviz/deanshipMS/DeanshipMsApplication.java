86763b1d24d8d5882f66a78fba02d79cc8948b92
Parviz Asadli <86598426+ParvizAsad@users.noreply.github.com>
2023-02-28 16:49:48 +0400

    Init

---
+++ Back/deanshipMS/src/main/java/com/parviz/deanshipMS/DeanshipMsApplication.java
@@ -0,0 +1,13 @@
+package com.parviz.deanshipMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeanshipMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeanshipMsApplication.class, args);
+	}
+
+}
---
+++ Back/deanshipMS/src/test/java/com/parviz/deanshipMS/DeanshipMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parviz.deanshipMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeanshipMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
