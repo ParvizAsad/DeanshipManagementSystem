3cc1c9b5371ce9b761838778bbeb95813903166d
Parviz Asadli <86598426+ParvizAsad@users.noreply.github.com>
2023-03-25 23:12:14 +0400

    Update Readme.md file

---
+++ Front/deanshipms-client/src/app/core/models/CreateLocation.ts
@@ -0,0 +1,8 @@
+export class CreateLocation {
+    name:string
+    // id:number  
+    constructor(name: '') {
+      this.name = name; 
+    //   this.id=id;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# DeanshipManagementSystem
+
+# Deanship Management system
+
+The project is under development
+
+![web_development.gif](./docs/web_development.gif)
+
